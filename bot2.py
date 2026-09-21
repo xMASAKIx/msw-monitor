a972222502c4bbd0faf9d4596197f1267a02cc16
xMASAKIx <110505678+xMASAKIx@users.noreply.github.com>
2026-09-21 20:30:10 +0800

    Update Discord webhook URL

--- bot2.py
+++ bot2.py
@@ -31,7 +31,7 @@ PLAYER_MAP = {
 }
 
 DEFAULT_IMAGE = "https://example.com/default.png"
-DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1497592013166608484/-bQDkOKmZBbxRMXwkmgQqrFsk4cdrtKIuKfVlxk81XeXwqalZ-9VliOuSC5wI1YMcuRT"
+DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1551570994974433450/VeaRWfv932MFRaYBB14fbWT90fG2-TrtK5yR0VH2nWz7HeDItUK8FapQK8ctsx0N6Xt2"
 
 # 建議調到 30 或 60 比較安全，但這邊先保留你原本的 15 試試看
 CHECK_INTERVAL = 15 
